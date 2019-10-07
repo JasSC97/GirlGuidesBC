fbde402a9fab49843a090e01ad5182befa725481
JasSC97 <jaskaran2247@gmail.com>
2019-10-06 22:32:10 -0700

    updated routing

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ const path = require('path');
 //All routes.
 const static = require('./static');
 const aboutPage = require('./about');
+app.use(static);
+app.use(aboutPage);
 
-
-app.use('', static);
-app.use('', aboutPage);
+//Hosting the index page.
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
